9411035bf475834a77d2e9e4091a7c9c6787ff23
Jerebaker <b.jeremiah1@gmail.com>
2024-04-08 14:27:48 -0500

    4/8

--- saveImages.jsx
+++ saveImages.jsx
@@ -219,7 +219,7 @@ function saveFiles(makeThumbs) {
 
 function processPromoFiles(t,lt) {
 
-	newDoc = documents.add(1280,720,72,"tempDoc", NewDocumentMode.RGB, DocumentFill.TRANSPARENT);
+	newDoc = documents.add(1620,720,72,"tempDoc", NewDocumentMode.RGB, DocumentFill.TRANSPARENT);
 	app.activeDocument = doc;
 	var assetPath = "~/Documents/GitHub/Photoshop-Scripts/Assets/Title Graphics/";
 
@@ -230,8 +230,8 @@ function processPromoFiles(t,lt) {
 	var tGraphicPsdText = importImage(assetPath+"tGraphicPsdText.png",newDoc,'tGraphicPsdText',329,554,null,null,false);
 	
 	var tGraphicCheckerBG = importImage(assetPath+"tGraphicCheckerBG.png",newDoc,'tGraphicCheckerBG',0,0,null,null,true);
-	var ltHeight = Math.round(getHeight(lt) * (1280/2880));
-	var tGraphicCheckerLayout = importImage(lt,newDoc,'tGraphicCheckerLayout',0,480-(ltHeight-240),1280,ltHeight,true);
+	var ltHeight = Math.round(getHeight(lt) * (1620/2880));
+	var tGraphicCheckerLayout = importImage(lt,newDoc,'tGraphicCheckerLayout',0,480-(ltHeight-240),1620,ltHeight,true);
 	
 	saveImage('_Images','L3_TH_'+docName,'jpg');
 	
